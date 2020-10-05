1bd5c9c9db0f19a368b7a29eea75b6b72ac38949
abdoolly <abdallahgamal@Abdallahs-MacBook-Pro.local>
2020-10-05 04:04:54 +0200

    added arrow icon

---
+++ src/components/Arrow/Arrow.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { ReactComponent as ArrowIcon } from './arrow.svg';
+import './Arrow.css';
+
+const Arrow = ({ direction = 'right', size = 30 }) => {
+    const [color, setColor] = useState('#cecece');
+    const onMouseOver = () => setColor('black');
+    const onMouseOut = () => setColor('#cecece');
+
+    return (
+        <ArrowIcon
+            height={size}
+            width={size}
+            className={`arrow ${direction === 'right' ? 'right' : 'left'}`}
+            fill={color}
+            onMouseOver={onMouseOver}
+            onMouseOut={onMouseOut}
+        />
+    );
+};
+
+export default Arrow;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import BoldText from '../../components/BoldText/BoldText';
-import NormalText from '../../components/NormalText/NormalText';
-import NumSquare from '../../components/NumSquare/NumSquare';
+import Arrow from '../../components/Arrow/Arrow';
+import Calendar from '../../components/Calendar/Calendar';
 
 const Home = () => {
     return (
         <div>
-            <BoldText text="Hello world Bold" />
-            <NormalText text="Hello world Normal" />
-            <NumSquare text="7" />
+            <Calendar />
+            <Arrow />
         </div>
     );
 };
